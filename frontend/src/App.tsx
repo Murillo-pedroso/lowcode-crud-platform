485ce90417aab755c338206a3b7e8c16b7747c78
oMurillo_ <murillomonteirop@gmail.com>
2025-10-17 00:36:31 -0300

    iniciando front+modificações na api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,86 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+  Box,
+  Typography,
+} from "@mui/material";
+import Tables from "./pages/Tables"
+import TableData from "./pages/TableData";
 import axios from "axios";
 
 interface TableMetadata {
   _id: string;
   tableName: string;
-  fields: Array<{ name: string; type: string }>;
+  tableLabelName: string;
 }
 
-const Home: React.FC = () => {
-  // const [tables, setTables] = useState<TableMetadata[]>([]);
-  // const [loading, setLoading] = useState<boolean>(true);
+const drawerWidth = 240;
+
+const App: React.FC = () => {
+  const [tables, setTables] = useState<TableMetadata[]>([]);
 
   useEffect(() => {
     axios
-      .get("http://localhost:5000/tables/metadata/")
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.error("Erro ao buscar os metadados das tabelas:", error);
-      });
+      .get("http://localhost:5000/tables/metadata")
+      .then((res) => setTables(res.data))
+      .catch((err) => console.error("Erro ao carregar tabelas", err));
   }, []);
 
-  return <div></div>;
+  return (
+    <Router>
+      <Box sx={{ display: "flex" }}>
+        {/* Menu lateral */}
+        <Drawer
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            "& .MuiDrawer-paper": {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          variant="permanent"
+          anchor="left"
+        >
+          <Toolbar />
+          <List>
+            <ListItem>
+              <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                Menu
+              </Typography>
+            </ListItem>
+            <ListItem  component={Link} to="/tables">
+              <ListItemText primary="Tabelas" />
+            </ListItem>
+
+            {tables.map((t) => (
+              <ListItem      
+                key={t._id}
+                component={Link}
+                to={`/tables/${t._id}`}
+              >
+                <ListItemText primary={t.tableLabelName} />
+              </ListItem>
+            ))}
+          </List>
+        </Drawer>
+
+        {/* Conteúdo */}
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <Toolbar />
+          <Routes>
+            <Route path="/tables" element={<Tables />} />
+            <Route path="/tables/:tableId" element={<TableData />} />
+          </Routes>
+        </Box>
+      </Box>
+    </Router>
+  );
 };
 
-export default Home;
+export default App;
